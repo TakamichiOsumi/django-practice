59d47da87e5649367aa400868bd357b2905eb873
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-05-31 17:22:09 +0900

    Created CustomUser, Post and Like.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ImageSpecField, ProcessedImageField
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    description = models.TextField(verbose_name = 'description',
+                                   null = True,
+                                   blank = True)
+    photo = models.ImageField(verbose_name = 'photo',
+                              null = True,
+                              blank = True,
+                              upload_to = 'images/')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        verbose_name_plural = 'CustomUser'
--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill, ResizeToFit
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('accounts.CustomUser',
+                               on_delete = models.CASCADE)
+    text = models.TextField(verbose_name = 'text')
+    photo = models.ImageField(verbose_name = 'photo',
+                              blank = True,
+                              null = True,
+                              upload_to = 'images/')
+    post_photo = ImageSpecField(source = 'photo',
+                                processors = [ResizeToFit(1080, 1080)],
+                                format = 'JPEG',
+                                options = { 'quality' : 60 })
+    created_at = models.DateTimeField(auto_now_add = True,
+                                      blank = True)
+
+    def get_like(self):
+        likes = Like.objects.filter(post = self)
+        return [like.user for like in likes]
+
+class Like(models.Model):
+    user = models.ForeignKey('accounts.Customuser',
+                             on_delete = models.CASCADE)
+    post = models.ForeignKey('Post',
+                             on_delete = models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')
