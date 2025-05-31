bb9f4e94ee4e26ec39a633fd6dda2433108c90d9
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-01 05:36:26 +0900

    Editing and showing user profile are enabled.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import SignupView, ProfileEditView
+from .views import SignupView, ProfileEditView, ProfileDetailView
 
 app_name = 'accounts'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('signup/', SignupView.as_view(), name = 'signup'),
     path('edit/<int:pk>',   ProfileEditView.as_view(), name = 'edit'),
+    path('detail/<int:pk>', ProfileDetailView.as_view(), name = 'detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-# from django.contrib.auth.models import User
 from .models import CustomUser
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from django.contrib.messages.views import SuccessMessageMixin
@@ -14,11 +13,32 @@ class SignupView(CreateView):
     model = CustomUser
     form_class = SignupForm
     template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('accounts:detail')
 
-class ProfileEditView(UpdateView):
+    def get_success_url(self):
+        return reverse('accounts:detail',
+                       kwargs = { 'pk': self.object.pk })
+
+class ProfileEditView(LoginRequiredMixin,
+                      SuccessMessageMixin, UpdateView):
     model = CustomUser
     fields = ('username', 'description', 'photo', )
     template_name = 'accounts/edit.html'
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('accounts:detail')
     success_message = 'Your profile has been updated'
+
+    def get_success_url(self):
+        return reverse('accounts:detail',
+                       kwargs = { 'pk': self.object.pk })
+
+class ProfileDetailView(LoginRequiredMixin,
+                        generic.DetailView):
+    model = CustomUser
+    template_name = 'accounts/detail.html'
+    success_url = reverse_lazy('accounts:detail')
+
+    def get_success_url(self):
+        return reverse('accounts:detail',
+                       kwargs = { 'pk': self.object.pk })
+
+detail = ProfileDetailView.as_view()
