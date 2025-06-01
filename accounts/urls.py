003634750c61eb9a305068a7c10f088f75ceaca1
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-01 19:01:26 +0900

    Fixed a bug not to be able to see customuser profile by accounts:detail.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/',  LoginView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('signup/', SignupView.as_view(), name = 'signup'),
-    path('edit/<int:pk>',   ProfileEditView.as_view(), name = 'edit'),
+    path('edit/<int:pk>', ProfileEditView.as_view(), name = 'edit'),
     path('detail/<int:pk>', ProfileDetailView.as_view(), name = 'detail'),
 ]
