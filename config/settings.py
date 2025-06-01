c86cdaecf62b8f3736877f21e630744c88c97303
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-01 14:38:38 +0900

    Introduced bootstrap5 and conducted some tests.

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'accounts.apps.AccountsConfig',
     'timeline.apps.TimelineConfig',
 ]
