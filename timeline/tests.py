dbf15f232ae946e56700e14f92eea01df9e93cf5
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-03 22:08:13 +0900

    Added the initial test for successful login.

--- timeline/tests.py
+++ timeline/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from .models import Post
+from accounts.models import CustomUser
 
 # Create your tests here.
+class TimelineTestCase(TestCase):
+
+    def test_index(self):
+        client = Client()
+        self.test_user = get_user_model().objects._create_user(
+            username = 'testuser_foo',
+            email = 'testuser_foo@example.com',
+            password = 'password_foo')
+        client.login(username = 'testuser_foo', password = 'password_foo')
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
