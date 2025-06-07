4f827c8783d588a66009212acb3bbfaea19e3cea
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-07 15:35:32 +0900

    Define new CustomUser fields using ArrayField to write the logic of following/follower.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from imagekit.models import ImageSpecField, ProcessedImageField
 
 class CustomUser(AbstractUser):
@@ -13,5 +14,13 @@ class CustomUser(AbstractUser):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    # Must be the type same as the primary key used by
+    # the CustomUser id.
+    following = ArrayField(models.BigIntegerField(),
+                           blank = True,
+                           default = list)
+    follower = ArrayField(models.BigIntegerField(),
+                          blank = True,
+                          default = list)
     class Meta:
         verbose_name_plural = 'CustomUser'
