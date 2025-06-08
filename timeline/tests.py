52b03b74f3b5efa891e419e6d950169e9654aec6
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-08 09:48:29 +0900

    Added the test of post feature with single user.

--- timeline/tests.py
+++ timeline/tests.py
@@ -6,6 +6,13 @@ from accounts.models import CustomUser
 # Create your tests here.
 class TimelineTestCase(TestCase):
 
+    def gen_user(self, username, password, email):
+        test_user = get_user_model().objects._create_user(
+            username = username,
+            email = email,
+            password = password)
+        return test_user
+
     def test_timeline_index_without_login(self):
         client = Client()
         response = client.get('/')
@@ -13,10 +20,21 @@ class TimelineTestCase(TestCase):
 
     def test_timeline_index_with_login(self):
         client = Client()
-        self.test_user = get_user_model().objects._create_user(
-            username = 'testuser_foo',
-            email = 'testuser_foo@example.com',
-            password = 'password_foo')
-        client.login(username = 'testuser_foo', password = 'password_foo')
+        test_user = self.gen_user('testuser_foo',
+                                  'password_foo_123',
+                                  'testuser_foo@example.com')
+        client.login(username = 'testuser_foo',
+                     password = 'password_foo_123')
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_timeline_post(self):
+        client = Client()
+        test_user = self.gen_user('testuser_foo',
+                                  'password_foo_123',
+                                  'testuser_foo@example.com')
+        client.login(username = 'testuser_foo',
+                     password = 'password_foo_123')
+        client.post('/create/', { 'text' : 'Hello World', 'photo': ''})
+        latest_post = Post.objects.latest('created_at')
+        self.assertEqual(latest_post.text, 'Hello World')
