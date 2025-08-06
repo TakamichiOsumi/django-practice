8d24c8180eb288d9b9669ee77b4632dc66d5e364
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-08-06 22:59:18 +0900

    Added the test for django-axes.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.test import TestCase, Client, override_settings
+from django.urls import reverse
 from accounts.models import CustomUser
+
+from config.settings import AXES_FAILURE_LIMIT
 
 # Create your tests here.
 class AccountsTestCase(TestCase):
@@ -16,6 +19,19 @@ class AccountsTestCase(TestCase):
         client = Client()
         response = client.get('/')
         self.assertEqual(response.status_code, 302)
+
+    def test_brute_force_attacks(self):
+        client = Client()
+        login_url = reverse('accounts:login')
+        auth_info = {'username': 'invalid_user',
+                     'password': 'invalid_password' }
+        # Login attemps before the account lockout
+        for i in range(AXES_FAILURE_LIMIT - 1):
+            response = self.client.post(login_url, auth_info)
+            self.assertEqual(response.content.find(b'Account Lockout'), -1)
+        # Trigger the account lockout by one more login failure
+        response = self.client.post(login_url, auth_info)
+        self.assertTrue(response.content.find(b'Account Lockout') >= 0)
 
     @override_settings(AXES_ENABLED = False)
     def test_timeline_login_and_logout(self):
